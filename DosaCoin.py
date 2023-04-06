7f6471b3f6617490f53e63fab73e5467a86bff76
NandanHemanth <theuniverseisflat101@gmail.com>
2023-04-06 22:14:51 +0530

    new block creation

--- DosaCoin.py
+++ DosaCoin.py
@@ -22,5 +22,19 @@ class Blockchain:
         self.new_block(previous_hash="1", proof="200")
 
     def new_block(self, proof, previous_hash):
-        
+
+        #Each BLock consists of 5 fields which define a unique block
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+            'proof': proof
+        }
+
+        self.current_transactions = []
+        self.chain.append(block)
+        return block
+    
+    def new_transaction(self,)
 
