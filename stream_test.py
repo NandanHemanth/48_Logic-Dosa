2a9ccfb25cefda5e7517c950c6e3ec1659c2a97e
Nandan H <theuniverseisflat101@gmail.com>
2023-04-07 15:47:33 +0530

    edumize

--- stream_test.py
+++ stream_test.py
@@ -1,33 +1,59 @@
 import streamlit as st
 import subprocess
 
-if st.button('Run A* Algorithm'):
-    subprocess.run(['python', 'astar.py'])
-
-url = st.text_input('Enter Path To File:')
-#command = ['python','mcqg.py','--url',url]
-print(url)
-if st.button('Run'):
-    import mcqg
-    mcqg.main(url)
+# Set up the app layout
+st.set_page_config(page_title="EDUMIZE", page_icon=":books:")
+st.title("EDUMIZE")
+st.header("By Logic Dosa\n")
 
 def open_file(url):
-    with open(url, 'r') as file:
+    with open(url, "r") as file:
         file_contents = file.read()
     return file_contents
 
-if st.button('Open File'):
-    url='mcq.txt'
-    file_contents=open_file(url)
-    st.text(file_contents)
+def open_file1(text):
+    with open(text, 'r') as file:
+        file_contents = file.read()
+    return file_contents
 
-if st.button('Show File Answers'):
-    url='mcq_ans.txt'
-    file_contents=open_file(url)
-    st.text(file_contents)
+st.markdown("Welcome to EDUMIZE, an educational app that provides various tools for learning.")
 
-if st.button('Video Chat 1'):
-    subprocess.run(['python','video_chat1.py'])
+menu = ["Visual Learning", "Text Summarizer", "MCQ Generator", "Study Room"]
+choice = st.sidebar.selectbox("Select a tool:", menu)
 
-if st.button('Video Chat 2'):
-    subprocess.run(['python','video_chat2.py'])
+if choice == "Visual Learning":
+    st.subheader("Visual Learning")
+    if st.button("Run A* Algorithm"):
+        subprocess.run(["python", "astar.py"])
+elif choice == "Text Summarizer":
+    st.subheader("Text Summarizer")
+    text = st.text_area("Enter Text:", height=10)
+    if st.button("Run Text Summarizer"):
+        import summary
+        summary.main1(text)
+    if st.button("Print File"):
+        text="summarized.txt"
+        file_contents = open_file1(text)
+        st.text(file_contents)
+elif choice == "MCQ Generator":
+    st.subheader("MCQ Generator")
+    url = st.text_input("Enter Path To File:")
+    if st.button("Run"):
+        import mcqg
+        mcqg.main(url)
+    if st.button("Open File"):
+        url = "mcq.txt"
+        file_contents = open_file(url)
+        st.text(file_contents)
+    if st.button("Show File Answers"):
+        url = "mcq_ans.txt"
+        file_contents = open_file(url)
+        st.text(file_contents)
+elif choice == "Study Room":
+    st.subheader("Study Room")
+    container = st.container()
+    with container:
+        if st.button("Video Chat 1"):
+            subprocess.run(["python", "video_chat1.py"])
+        if st.button("Video Chat 2"):
+            subprocess.run(["python", "video_chat2.py"])
